02b42c69158221e8f1171d2ca0e543b3604e6958
Austin Conn <austinc3030@gmail.com>
2024-01-22 16:03:15 -0500

    Add some static analysis artifacts

--- hubs/tools/dump_partitions.sh
+++ hubs/tools/dump_partitions.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-esp32_image_parser.py dump_partition factory_state.bin -partition otadata -output otadata.bin
-esp32_image_parser.py dump_partition factory_state.bin -partition phy_init -output phy_init.bin
-esp32_image_parser.py dump_partition factory_state.bin -partition ota_0 -output ota_0.bin
-esp32_image_parser.py dump_partition factory_state.bin -partition ota_1 -output ota_1.bin
-esp32_image_parser.py dump_partition factory_state.bin -partition nvs -output nvs.bin
-esp32_image_parser.py dump_partition factory_state.bin -partition storage0 -output storage0.bin
-esp32_image_parser.py dump_partition factory_state.bin -partition ktt -output ktt.bin
+../../../../esp32_image_parser/esp32_image_parser.py dump_partition factory_state.bin -partition otadata -output otadata.bin
+../../../../esp32_image_parser/esp32_image_parser.py dump_partition factory_state.bin -partition phy_init -output phy_init.bin
+../../../../esp32_image_parser/esp32_image_parser.py dump_partition factory_state.bin -partition ota_0 -output ota_0.bin
+../../../../esp32_image_parser/esp32_image_parser.py dump_partition factory_state.bin -partition ota_1 -output ota_1.bin
+../../../../esp32_image_parser/esp32_image_parser.py dump_partition factory_state.bin -partition nvs -output nvs.bin
+../../../../esp32_image_parser/esp32_image_parser.py dump_partition factory_state.bin -partition storage0 -output storage0.bin
+../../../../esp32_image_parser/esp32_image_parser.py dump_partition factory_state.bin -partition ktt -output ktt.bin
 
-esp32_image_parser.py dump_nvs factory_state.bin -partition nvs -nvs_output_type json >> nvs.json
+../../../../esp32_image_parser/esp32_image_parser.py dump_nvs factory_state.bin -partition nvs -nvs_output_type json >> nvs.json
